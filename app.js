9fc3666756b65e8f3089e52848d98da9cb432e13
Zohaib Ali <137928612+Zohaib6234@users.noreply.github.com>
2023-10-22 01:13:40 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+// // chapter 5 
+// // quesion 1
+
+// // var num = 3 + 5
+// // ++num
+
+// // // document.write(num)
+// // // console.log(num)
+// // num = num * 3
+// // console.log(num)
+
+// // question 2 
+
+// // console.log(num*4-30+10)
+
+// // question 3
+
+// // console.log(num)
+
+// // console.log(num + 2 + 3 + 3)
+// // --num
+// // console.log(num + 2 + 3 + 3)
+
+// // question 4
+// // var cost = 600
+// // var Qty = +prompt("How Many Ticket You By")
+
+// // // console.log(cost * Qty)
+// // document.write("Total Cost is"+" "+ cost * Qty)
+
+// // question 5
+
+// // var tbl = 4
+// // var qty = +prompt("Enter Value")
+// // document.write("Table of 4" + "<br> <br>")
+// // document.write("4 X 1 = " + +tbl * 1 + "<br>")
+// // document.write("4 X 2 = " + tbl * 2 + "<br>")
+// // document.write("4 X 3 = " + tbl * 3 + "<br>")
+// // document.write("4 X 4 = " + tbl * 4 + "<br>")
+// // document.write("4 X 5 = " + tbl * 5 + "<br>")
+// // document.write("4 X 6 = " + tbl * 6 + "<br>")
+// // document.write("4 X 7 = " + tbl * 7 + "<br>")
+// // document.write("4 X 8 = " + tbl * 8 + "<br>")
+// // document.write("4 X 9 = " + tbl * 9 + "<br>")
+// // document.write("4 X 10 = " + tbl * 10 + "<br>")
+
+
+// // question 7
+
+// // var a = "Price of item 1 is 650"
+// // var b = "Price of item 2 is 100"
+// // var c = "Ordered Quantity of item 1 is 3"
+// // var d = "Ordered Quantity of item 2 is 7"
+// // var e = "Shipping charges 100"
+// // var Total = "Total cost of your order is " 
+// // document.write(a + "<br>" + b + "<br>" + c + "<br>" + d + "<br>" + e + "<br>" + "<br>")
+// // document.write("Total cost of your order is ")
+// // document.write((650 * 3) + (100 * 7) + 100)
+
+// // question 8
+
+// // document.write("<h1>MARKS SHEET</h1>" + "<br>")
+// // document.write("Total Marks: 980" + "<br>")
+// // document.write("Marks Obtained: 804" + "<br>" + "<br>" + "<br>")
+// // document.write("Percentage : ")
+// // document.write((804 / 980) * 100)
+
+// // var head = "<h1>MARKS SHEET</h1>" + "<br>"
+// // var total = "Total Marks: 980" + "<br>"
+// // var obt = "Marks Obtained: 804" + "<br>" + "<br>" + "<br>"
+// // var prst = "Percentage : "
+// // var num = (804 / 980) * 100
+// // document.write(head + total + obt + prst + num)
+
+
+// // question 9
+// // var doller = "10 US Dollers:"
+// // var riyal = "25 Saudi Riyal:"
+// // var doll = 275.00 * 10
+// // var riy = 73.31 * 25
+// // document.write("<h1>Currency in PKR</h1>  <br>  <br>  <br>")
+// // document.write("<br> U.S Doller : 275.00 PKR <br>")
+// // document.write("Saudi Riyal : 73.31 PKR <br>  <br> <br> <br>")
+// // // document.write(doller + (275.00 * 10) + "<br>" + riyal + (73.31 * 25))
+// // document.write(doller + doll + "<br>" + riyal + riy)
+// // document.write("<br> Total :  " + (doll + riy))
+
+
+// document.write("<h1>Age Calcolator</h1> <br> <br> <br> <br>")
+
+// var a = " Current Year : "
+// var b = "Birth Year : "
+// var current = 2023
+// var birth = 1993
+// var age = "Your Age is "
+
+// document.write(a + current + "<br>" + b + birth + "<br>" + age + (current - birth))
+
